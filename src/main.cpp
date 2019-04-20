c5ef6fbc389a139cfe8cd123d339e347c7eb91d2
nalane <nathaniel.patrick.lane@gmail.com>
2019-04-19 18:14:14 -0600

    Modified to use command line arg as filepath

--- src/main.cpp
+++ src/main.cpp
@@ -10,64 +10,65 @@ typedef CGAL::Simple_cartesian<double> Kernel;
 typedef Kernel::Point_2 Point_2;
 
 using namespace std;
-void convert();
 
-int main()
-{
-  convert();
-  return 0;
-}
-
-  void convert(){
+void convert(string filepath) {
     string line;
-    ifstream myfile ("/home/yinyalan/Downloads/dataset-sample/files/file-000000.dat");
+    ifstream myfile (filepath.c_str());
     vector<Point_2> p;
 
     if(myfile.is_open())
     {
-      while(getline(myfile, line))
-      {
-        const double pi = 3.1415926535897;
-        double mapWidth = 600;
-        double mapHeight = 800;
+        while(getline(myfile, line))
+        {
+            const double pi = 3.1415926535897;
+            double mapWidth = 600;
+            double mapHeight = 800;
 
-        double x, y;
-        double x_mapped, y_mapped;
-        double latRad, mercN;
+            double x, y;
+            double x_mapped, y_mapped;
+            double latRad, mercN;
 
-        //cout << line << '\n' << endl;
-        myfile >> x;
-        myfile >> y;
+            //cout << line << '\n' << endl;
+            myfile >> x;
+            myfile >> y;
 
-        //turn off scientific notation
-        cout.setf(ios::fixed, ios::floatfield);
-        cout.setf(ios::showpoint);
+            //turn off scientific notation
+            cout.setf(ios::fixed, ios::floatfield);
+            cout.setf(ios::showpoint);
 
-        cout.precision(3);
+            cout.precision(3);
 
-        //get x value
-        //x_mapped = (x+180)*(mapWidth/360);
-        x_mapped = fmod(mapWidth*(x+180)/360, (mapWidth+(mapWidth)/2));
+            //get x value
+            //x_mapped = (x+180)*(mapWidth/360);
+            x_mapped = fmod(mapWidth*(x+180)/360, (mapWidth+(mapWidth)/2));
 
-        //convert from degree to radians
-        latRad = y*pi/180;
+            //convert from degree to radians
+            latRad = y*pi/180;
 
-        //get y
-        mercN = log(tan(pi/4)+(latRad/2));
-        y_mapped = (mapHeight/2)-(mapWidth*mercN/(2*pi));
+            //get y
+            mercN = log(tan(pi/4)+(latRad/2));
+            y_mapped = (mapHeight/2)-(mapWidth*mercN/(2*pi));
 
 
-        //cout << x << " " << y << endl;
-        //p.push_back(Point_2(x,y));
-        p.push_back(Point_2(x_mapped, y_mapped));
-      }
-      myfile.close();
+            //cout << x << " " << y << endl;
+            //p.push_back(Point_2(x,y));
+            p.push_back(Point_2(x_mapped, y_mapped));
+        }
+        myfile.close();
     }
 
+    for(Point_2 point : p){
+        cout << point << endl;
+    }
+}
 
-    vector<Point_2>::iterator it;
-    for(it = p.begin(); it !=p.end(); it++){
-      cout << *it << endl;
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " file.dat" << endl;
+        return 1;
     }
 
-  }
+    convert(argv[1]);
+    return 0;
+}
