ea1170c81ec73af42bb66361b9923d72d71f5545
nalane <nathaniel.patrick.lane@gmail.com>
2019-04-28 16:49:59 -0600

    Pushing all functions together

--- src/main.cpp
+++ src/main.cpp
@@ -46,74 +46,73 @@ void print_column_set(std::set<std::vector<bool>> set){
 
 std::set<std::vector<bool>> arrangement_creation(std::vector<Trait_Point_2> query_curve, double radius){
 
-  Arrangement_2 arr;
+    Arrangement_2 arr;
 
-  std::set<std::vector<bool>> column_set;
+    std::set<std::vector<bool>> column_set;
 
-  // insert all circles which are centered at each point of query_curve.
-  CGAL::Exact_rational sqr_r = CGAL::Exact_rational(pow(radius, 2));
-  for(unsigned i = 0; i < query_curve.size(); i ++){
+    // insert all circles which are centered at each point of query_curve.
+    CGAL::Exact_rational sqr_r = CGAL::Exact_rational(pow(radius, 2));
+    for(unsigned i = 0; i < query_curve.size(); i ++){
 
-    Circle_2 circ = Circle_2(Double_Point_2(CGAL::to_double(query_curve[i].x()), CGAL::to_double(query_curve[i].y())), sqr_r, CGAL::CLOCKWISE);
-    Curve_2 cv = Curve_2(circ);
-    insert(arr, cv);
-  }
-
-  Naive_pl naive_pl(arr);
-  // face to column
-  Arrangement_2::Face_const_iterator fit;
-  for (fit = arr.faces_begin(); fit != arr.faces_end(); ++fit){
-    if (fit -> is_unbounded() == false){
-      std::vector<Trait_Point_2> face_point_vector;
-      Arrangement_2::Ccb_halfedge_const_circulator  circ = fit->outer_ccb();
-      Arrangement_2::Ccb_halfedge_const_circulator  curr = circ;
-      Arrangement_2::Halfedge_const_handle          he;
-
-      face_point_vector.push_back(curr->source()->point());
-      do
-      {
-        he = curr;
-        face_point_vector.push_back(he->target()->point());
-        ++curr;
-      } while (curr != circ);
-
-      // 2.
-      bool inside = false;
-      int index_one = 0;
-      int index_two = 1;
-      int vector_length = face_point_vector.size() - 1;
-      Trait_Point_2 mid_point;
-      while(inside == false){
-        Trait_Point_2 left = face_point_vector[index_one];
-        Trait_Point_2 right = face_point_vector[index_two];
-        mid_point = mid_point_creatation(left, right);
-
-        CGAL::Arr_point_location_result<Arrangement_2>::Type obj = naive_pl.locate(mid_point);
-        Arrangement_2::Face_const_handle* f;
-        if (f = boost::get<Arrangement_2::Face_const_handle>(&obj)){
-          if(fit == *f){
-            //std::cout<<"   true  " << std::endl;
-            inside = true;
-          }
-        }
-        index_two += 1;
-        if (index_two = vector_length){
-          index_one += 1;
-          index_two = index_one + 1;
-        }
-        if (index_one == vector_length){
-          std::cout<< "big error!!!!!!"<<std::endl;
-          return column_set;
-        }
-
-      }
-
-      std::vector<bool> column;
-      column = column_creation(query_curve, mid_point, radius);
-      column_set.insert(column);
-
+        Circle_2 circ = Circle_2(Double_Point_2(CGAL::to_double(query_curve[i].x()), CGAL::to_double(query_curve[i].y())), sqr_r, CGAL::CLOCKWISE);
+        Curve_2 cv = Curve_2(circ);
+        insert(arr, cv);
     }
-  }
+
+    Naive_pl naive_pl(arr);
+    // face to column
+    Arrangement_2::Face_const_iterator fit;
+    for (fit = arr.faces_begin(); fit != arr.faces_end(); ++fit){
+        if (fit -> is_unbounded() == false){
+            std::vector<Trait_Point_2> face_point_vector;
+            Arrangement_2::Ccb_halfedge_const_circulator  circ = fit->outer_ccb();
+            Arrangement_2::Ccb_halfedge_const_circulator  curr = circ;
+            Arrangement_2::Halfedge_const_handle          he;
+
+            face_point_vector.push_back(curr->source()->point());
+            do
+            {
+                he = curr;
+                face_point_vector.push_back(he->target()->point());
+                ++curr;
+            } while (curr != circ);
+
+            // 2.
+            bool inside = false;
+            int index_one = 0;
+            int index_two = 1;
+            int vector_length = face_point_vector.size() - 1;
+            Trait_Point_2 mid_point;
+            while(inside == false){
+                Trait_Point_2 left = face_point_vector[index_one];
+                Trait_Point_2 right = face_point_vector[index_two];
+                mid_point = mid_point_creatation(left, right);
+
+                CGAL::Arr_point_location_result<Arrangement_2>::Type obj = naive_pl.locate(mid_point);
+                Arrangement_2::Face_const_handle* f;
+                if (f = boost::get<Arrangement_2::Face_const_handle>(&obj)){
+                    if(fit == *f){
+                        //std::cout<<"   true  " << std::endl;
+                        inside = true;
+                    }
+                }
+                index_two += 1;
+                if (index_two = vector_length){
+                    index_one += 1;
+                    index_two = index_one + 1;
+                }
+                if (index_one == vector_length){
+                    std::cout<< "big error!!!!!!"<<std::endl;
+                    return column_set;
+                }
+            } 
+
+            std::vector<bool> column;
+            column = column_creation(query_curve, mid_point, radius);
+            column_set.insert(column);
+
+        }
+    }
     print_column_set(column_set);
     return column_set;
     // print arrangement.
@@ -121,12 +120,12 @@ std::set<std::vector<bool>> arrangement_creation(std::vector<Trait_Point_2> quer
 }
 
 std::vector<Trait_Point_2> point_convertor(curve curve){
-  std::vector<Trait_Point_2> result;
-  std::vector<Double_Point_2> points = curve.get_curve();
-  for (unsigned i = 0; i < points.size(); i++){
-    result.push_back(Trait_Point_2(points[i].x(), points[i].y()));
-  }
-  return result;
+    std::vector<Trait_Point_2> result;
+    std::vector<Double_Point_2> points = curve.get_curve();
+    for (unsigned i = 0; i < points.size(); i++){
+        result.push_back(Trait_Point_2(points[i].x(), points[i].y()));
+    }
+    return result;
 }
 
 map<size_t, curve> get_curves(string filename) {
@@ -159,10 +158,10 @@ map<size_t, curve> get_curves(string filename) {
             // Get the coordinates in each line
             while (datain >> x >> y >> pointIndex >> curveIndex) {
                 points.push_back(Double_Point_2(x, y));
-                cout << points.back() << endl;
+                //cout << points.back() << endl;
             }
 
-            cout << points.size();
+            //cout << points.size();
 
             datain.close();
             curves[curveIndex] = curve(points);
@@ -173,10 +172,15 @@ map<size_t, curve> get_curves(string filename) {
     return curves;
 }
 
+vector<size_t> filter(set<vector<bool>> column_set, map<size_t, vector<vector<bool>>> matrices) {
+    vector<size_t> curve_ids;
+    return curve_ids;
+}
 
 int main(int argc, char** argv) {
     string datafile = "../data/dataset.txt";
     int queryIndex = 0;
+    double frechetDistance = 1.0;
 
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
@@ -184,6 +188,8 @@ int main(int argc, char** argv) {
                 datafile = argv[++i];
             else if (strcmp(argv[i], "-q") == 0)
                 queryIndex = atoi(argv[++i]);
+            else if (strcmp(argv[i], "-f") == 0)
+                frechetDistance = stod(argv[++i]);
         }
     }
 
@@ -191,27 +197,30 @@ int main(int argc, char** argv) {
 
     // Step 1.5: Get query curve
     curve q = curves[queryIndex];
-
-    //convert the query curve to Trait_Point_2
-    std::vector<Trait_Point_2> query_curve_converted =  point_convertor(q);
-
-    //create arrangement
-    std::set<std::vector<bool>> column_set_result = arrangement_creation(query_curve_converted, 10.0);
-
-    //get the free_space_matrix for the query curve
-    //vector<vector<bool>> arr = q.free_space_matrix(q, 10.0);
-
-
     curves.erase(queryIndex);
 
     // Step 2: free space: map<curve, matrix>
-    // Step 3: kernel -> trait_2
-    // Step 4: get column set
-    // Step 5: Filter
+    map<size_t, vector<vector<bool>>> matrices;
+    for (auto p : curves) {
+        matrices[p.first] = p.second.free_space_matrix(q, frechetDistance);
+    }
 
-    //for (auto p : curves) {
-        //p.second.print();
-    //}
+    // Step 3: kernel -> trait_2
+    vector<Trait_Point_2> query_curve_converted =  point_convertor(q);
+
+    // Step 4: get column set
+    set<vector<bool>> column_set_result =
+        arrangement_creation(query_curve_converted, frechetDistance);
+
+    // Step 5: Filter
+    vector<size_t> curve_ids = filter(column_set_result, matrices);
+
+    // Step 6: Print result
+    cout << "The folowing curves are within a Frechet distance " <<
+        frechetDistance << " of curve " << queryIndex << "\n";
+    for (size_t id : curve_ids) {
+        cout << id << "\n";
+    }
 
     return 0;
 }
